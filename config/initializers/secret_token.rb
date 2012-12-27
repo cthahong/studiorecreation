2217768849d2dccccbe0b085ac6d10a36187100b
kit thahong <cthahong@gmail.com>
2012-12-27 15:55:09 -0500

    rails app first coomit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudioRecreation::Application.config.secret_token = 'efd16df43fb163d376286272be0a619ec770ab270b19815caa66d918db982a99bab329c7ff76f4c0acd087384a9d7649503d05f72afc5760ac8372f7c11a8ae6'
